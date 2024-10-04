53d38436fb1a5cd2bb1320f6ac3af66b23ed2a2a
Виталий Беяр <vitaliy.beyar@gmail.com>
2024-10-04 13:15:30 -0230

    feat: settings for herocu

--- packages/frontend/src/utils/filter.tododata.ts
+++ packages/frontend/src/utils/filter.tododata.ts
@@ -1,10 +1,6 @@
 import { TodoType } from '~typings/todo.type';
 
 export const filterTodoData = (todo: TodoType): TodoType => {
-	const {
-		createdAt: _createdAt,
-		updatedAt: _updatedAt,
-		...filteredTodo
-	} = todo;
+	const { ...filteredTodo } = todo;
 	return filteredTodo;
 };
